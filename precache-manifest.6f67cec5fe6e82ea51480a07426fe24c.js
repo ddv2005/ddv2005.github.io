b9efc0e27dc95bd731575482a51f5d2c760bd135
unknown <C:\Users\Dmitry\AppData\Roaming\The Bat!>
2020-12-05 17:56:08 -0500

    HTML pages

---
+++ precache-manifest.6f67cec5fe6e82ea51480a07426fe24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf41b553e95e14f3ebfb8fc241b5dd82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e1833b1b562c0ae9a07",
+    "url": "/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1833b1b562c0ae9a07",
+    "url": "/static/js/main.577406e1.chunk.js"
+  },
+  {
+    "revision": "dab82071668f2702c64d",
+    "url": "/static/js/runtime-main.ab6486c8.js"
+  }
+]);
